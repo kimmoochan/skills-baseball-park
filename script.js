d1d6deb2904e90543e1b23116297cf80fd6021c6
kimmoochan <kimfkgml77@gmail.com>
2025-01-14 20:54:41 +0900

    가로 세로 차트 자바스크립트 제작

--- script.js
+++ script.js
@@ -14,16 +14,46 @@ async function chartView() {
     return data;
 }
 
+let maxCount = 0;
+
+// 가로 차트 띄우기
 async function widthChart() {
     const chartElem = document.querySelector("#chartDiv");
     chartElem.innerHTML = "";
     chartElem.className = "chart_width"
     const data = await chartView();
     Object.entries(data).forEach(([time,count])=>{
+        if(count>= 500){
+            maxCount = 500;
+        } else{
+            maxCount = count;
+        }
         chartElem.innerHTML += `
         <div class="width_parents">
           <p>${time}</p>
-          <div class="width" style="width: ${count}px;">${count}명</div>
+          <div class="width" style="width: ${maxCount}px;">${count}명</div>
+        </div>`
+    })
+    
+}
+
+
+// 세로 차트 띄우기
+async function heightChart() {
+    const chartElem = document.querySelector("#chartDiv");
+    chartElem.innerHTML = "";
+    chartElem.className = "chart_height"
+    const data = await chartView();
+    Object.entries(data).forEach(([time,count])=>{
+        if(count>= 500){
+            maxCount = 500;
+        } else{
+            maxCount = count;
+        }
+        chartElem.innerHTML += `
+        <div class="height_parents">
+        <div class="height" style="height: ${maxCount}px;">${count}명</div>
+          <p>${time}</p>
         </div>`
     })
     
